b27b4822a777437e2c803fef08a81dd1a3aed7fc
Kunallvz <kunal.kuumarr@gmail.com>
2026-09-06 22:12:09 +0530

    day1.py

--- day1.py
+++ day1.py
@@ -1,3 +1,11 @@
 # Day 1 - Python practice: Hello World
 # Streak day 1
 print("Hello, world!")
+
+print(2 + 3) # Addition
+print(4 - 2) # Subtraction
+print(5 * 5) # Multiplication
+print(10 / 2) # Divison 
+print(10 % 3) # Remainder
+print(3 ** 2) #
+print(3 // 2) #
