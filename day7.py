4f71c187ec18b39cb0b39eaf10f5fadd68abe66d
Kunallvz <kunal.kuumarr@gmail.com>
2026-09-08 22:20:05 +0530

    day7.pyVariables in python

---
+++ day7.py
@@ -0,0 +1,21 @@
+first_name = 'kunal'
+last_name = 'kumar'
+country = 'India'
+city = 'Delhi'
+age = 20
+is_student = True
+skills = ['Html','Css','Javascript','python']
+#person_info = {
+#   'first_name': 'kunal'
+#   'last_name':'kumar'
+#   'country': 'India' 
+#}
+# printing the values 
+print('first_name:',first_name)
+print('last_name:',last_name)
+print('country:',country)
+print('city:',city)
+print('age')
+print('skills')
+print('student:',is_student) 
+#print('person_info:',person_info)
