7e84ef5fff8f5abb60280f20bb8b70a4e89ae071
Kunallvz <kunal.kuumarr@gmail.com>
2026-09-09 22:22:42 +0530

    day8.pyUsing list

--- day8.py
+++ day8.py
@@ -5,4 +5,25 @@ fruits = ['Banana','Apple','orange','Mango']
 fruits.append('Strawberry')
 print("fruits:",fruits)
 # using append 
+fruits.append('Dragon fruit')
+print(fruits)
+#------------------------------------------- 
+#insert
+fruits = ['Banana','Apple','orange','Mango']
+fruits.insert(2,'lime')
+print(fruits)
+#-------------------------------------------- 
+# remove 
+#fruits = ['Banana','Apple','orange','Mango']
+#fruits.remove()
+#print(fruits)
+
+#fruits.remove(0)
+#print(fruits)
+#del --------------------------------------------- 
+fruits = ['Banana','Apple','orange','Mango']
+del fruits[1]
+print(fruits)
+del fruits[2]
+print(fruits)# error expected 
 
