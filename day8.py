0dca3ab10b3c192c418f8850ee558924e3e7f56c
Kunallvz <kunal.kuumarr@gmail.com>
2026-09-09 22:08:24 +0530

    day8.pyUsing list

---
+++ day8.py
@@ -0,0 +1,8 @@
+hollow_list = list()
+print(len(hollow_list))
+# list of fruits 
+fruits = ['Banana','Apple','orange','Mango']
+fruits.append('Strawberry')
+print("fruits:",fruits)
+# using append 
+
