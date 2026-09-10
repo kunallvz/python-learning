dad8b9b7edce3f00610d3f4b340667b20fddcabf
Kunallvz <kunal.kuumarr@gmail.com>
2026-09-10 21:34:18 +0530

    day9.pyLearning tuple

---
+++ day9.py
@@ -0,0 +1,13 @@
+print("h3llo, world")
+empty_tuple = ()
+empty_tuple = tuple() 
+tpl = ('apple','sqeet','lily')
+len(tpl)
+tpl = ('item1', 'item2', 'item3')
+first_item = tpl[0]
+second_item = tpl[1]
+fruits = ('banana', 'orange', 'mango', 'lemon')
+first_fruit = fruits[0]
+second_fruit = fruits[1]
+last_index =len(fruits) - 1
+last_fruit = fruits[last_index]
