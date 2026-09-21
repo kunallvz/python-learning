c598870407010c1464ecee946c573bb2d99c46ab
Kunallvz <kunal.kuumarr@gmail.com>
2026-09-21 22:50:58 +0530

    day23.py:if else loop

--- day23.py
+++ day23.py
@@ -39,4 +39,11 @@ if num >= 0:
     print("Positive")
 else:
     print("Negative")
-    
+#---------------------------------- 
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
