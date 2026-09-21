0087fa08de595f18661a94e7d118fceffbb5689d
Kunallvz <kunal.kuumarr@gmail.com>
2026-09-21 22:52:16 +0530

    day23.py:if else loop

--- day23.py
+++ day23.py
@@ -46,4 +46,12 @@ if num % 2 == 0:
     print("Even")
 else:
     print("Odd")
+#---------------------------- 
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You can vote")
+else:
+    print("You cannot vote")
+#----------------------------- 
 
