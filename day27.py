f5c5e246e5268317909681a9147e1951d7d4a496
Kunallvz <kunal.kuumarr@gmail.com>
2026-09-22 22:29:04 +0530

    day27.py:Dictionary

--- day27.py
+++ day27.py
@@ -12,3 +12,14 @@ student["age"] = 21
 print("Student profile:", student)
 print("Name:", student["name"])
 print("Course:", student["course"])
+#------------------------------ 
+# 2. Add and update items in a dictionary
+student = {
+    "name": "Rahul",
+    "age": 15
+}
+
+student["class"] = 10
+student["age"] = 16
+
+print(student) 
