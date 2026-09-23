f203f509f104422c5c63a55c2a0e5e067753a8c1
Kunallvz <kunal.kuumarr@gmail.com>
2026-09-23 22:00:54 +0530

    day28.pyStreak dau 28 repractice

---
+++ day28.py
@@ -0,0 +1,16 @@
+#Day 28  python learning 
+# streak day 28 
+class Car:
+    def __init__(self, brand, color):
+        self.brand = brand
+        self.color = color
+
+    def show(self):
+        print("Brand:", self.brand)
+        print("Color:", self.color)
+
+
+car1 = Car("BMW", "Black")
+car1.show() 
+#--------------------------- 
+
