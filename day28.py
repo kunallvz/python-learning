e39317748788747de3e930c6b040f5022549c904
Kunallvz <kunal.kuumarr@gmail.com>
2026-09-23 22:25:33 +0530

    day28.pyStreak dau 28 repractice

--- day28.py
+++ day28.py
@@ -77,3 +77,14 @@ elif guess < secret:
     print("Too low!")
 else:
     print("Too high!") 
+#---------------------------------\ 
+word = input("Enter a word: ")
+vowels = "aeiou"
+count = 0
+
+for letter in word.lower():
+    if letter in vowels:
+        count += 1
+
+print("Number of vowels:", count) 
+
